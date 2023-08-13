6c3c3e429864c7eb89469670fdfbc8b8a391e170
rawan-mortaja <rawanmortaja077@gmail.com>
2023-08-13 20:57:40 +0300

    Rawan

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,8 +1,5 @@
 @props(['messages'])
-@php
-    $name = str_replace('[', '.', $name);
-    $name = str_replace(']', '', $name);
-@endphp
+
 
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
